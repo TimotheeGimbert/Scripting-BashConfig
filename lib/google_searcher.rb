dd25d94cbf7db741e9c281a345ce2fd8908cfad3
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-15 19:05:32 +0200

    performing

--- lib/google_searcher.rb
+++ lib/google_searcher.rb
@@ -1,9 +1,20 @@
+require 'launchy'
+
 def check_if_user_gave_input
   abort("mkdir: missing input") if ARGV.empty?
+end
+ 
+
+def get_url_from_ARGV(argv_array)
+  search_str = argv_array.join('+')
+  url = search_str.prepend('https://google.com/search?q=')
+  return url
 end
 
 def perform
   check_if_user_gave_input
+  url = get_url_from_ARGV(ARGV)
+  Launchy.open(url)
 end
 
 perform
